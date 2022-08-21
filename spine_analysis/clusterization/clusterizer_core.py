- fixed spherical harmonic metric calculation

spine_analysis/clusterization/clusterizer_core.py
@@ -69,12 +69,10 @@ class SpineClusterizer(ABC):
 
     def score(self) -> float:
         # TODO: change nan to something sensical
-        labels = self.get_labels()
         if self.num_of_clusters < 2 or self.sample_size - 1 < self.num_of_clusters:
             return float("nan")
         labels = self.get_labels()
         return silhouette_score(self._data, labels, metric=self.metric)
-        # return silhouette_score(self._data, labels, metric="euclidean")
 
     @abstractmethod
     def _fit(self, data: np.array) -> object:
@@ -110,7 +108,13 @@ class SpineClusterizer(ABC):
 
     def save(self, filename: str) -> None:
         with open(filename, "w") as file:
-            json.dump({"cluster_masks": self.cluster_masks}, file)
+            clusters = self.get_clusters()
+            output = {
+                "groups": {
+                    f"cluster_{i}": clusters[i] for i in range(len(clusters))},
+                "samples": [clusters[i][j] for i in range(len(clusters)) for j in range(len(clusters[i]))]
+            }
+            json.dump(output, file)
 
     def get_representative_samples(self, cluster_index: int,
                                    num_of_samples: int = 4,

spine_analysis/clusterization/kmeans_clusterizer.py
@@ -1,3 +1,5 @@
+from typing import Union, Callable
+
 import numpy as np
 from sklearn.cluster import KMeans
 
@@ -5,8 +7,8 @@ from spine_analysis.clusterization.clusterizer_core import SKLearnSpineClusteriz
 
 
 class KMeansSpineClusterizer(SKLearnSpineClusterizer):
-    def __init__(self, num_of_clusters: int, pca_dim: int = -1):
-        super().__init__(pca_dim=pca_dim)
+    def __init__(self, num_of_clusters: int, pca_dim: int = -1, metric: Union[str, Callable] = "euclidean"):
+        super().__init__(pca_dim=pca_dim, metric=metric)
         self.num_of_clusters = num_of_clusters
 
     def _sklearn_fit(self, data: np.array) -> object:

spine_analysis/shape_metric/approximation_metric.py
@@ -8,28 +8,29 @@ from numpy import real
 from scipy.special import sph_harm
 from sklearn.linear_model import LinearRegression
 
+from CGAL.CGAL_Kernel import Vector_3
 from CGAL.CGAL_Point_set_3 import Point_set_3
 from CGAL.CGAL_Poisson_surface_reconstruction import poisson_surface_reconstruction
 from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
 from spine_analysis.mesh.utils import _mesh_to_v_f
 from spine_analysis.shape_metric.metric_core import SpineMetric
 from spine_analysis.shape_metric.utils import polar2cart, cart2polar
-from spine_segmentation import point_2_list
+from spine_segmentation import point_2_list, list_2_point
 
 
 class ApproximationSpineMetric(SpineMetric, ABC):
-    _coefficients: List[float]
     _basis: List[callable]
     _bbox: np.ndarray
 
     def __init__(self, spine_mesh: Polyhedron_3 = None):
+        if spine_mesh is not None:
+            points = [point_2_list(point) for point in spine_mesh.points()]
+            self._bbox = np.array([np.min(points, axis=0),np.max(points, axis=0)])
         super().__init__(spine_mesh)
-        points = [point_2_list(point) for point in spine_mesh.points()]
-        self._bbox = np.array([np.min(points, axis=0),np.max(points, axis=0)])
 
     def show(self, steps_num: List[int] = None) -> widgets.Widget:
         if steps_num is None:
-            steps_num = [100, 100]
+            steps_num = [50, 50]
         mesh: Polyhedron_3 = self._get_mesh(steps_num)
         vertices, facets = _mesh_to_v_f(mesh)
         out = widgets.Output()
@@ -43,9 +44,13 @@ class ApproximationSpineMetric(SpineMetric, ABC):
 
     def surface_value(self, point: List[float]) -> float:
         res: float = 0
-        for a, f in zip(self._coefficients, self._basis):
+        for a, f in zip(self._value, self._basis):
             res += a * f(*point)
-        return res
+        return real(res)
+
+    def surface_equation(self, point: List[float]) -> float:
+        r = self.surface_value(point[:-1])
+        return r - point[-1]
 
     @abstractmethod
     def surface_norm(self, point: List[float], dx: float = 0.01) -> np.ndarray:
@@ -53,12 +58,10 @@ class ApproximationSpineMetric(SpineMetric, ABC):
 
 
 class SphericalGarmonicsSpineMetric(ApproximationSpineMetric):
-    def __init__(self, spine_mesh: Polyhedron_3 = None, m_range: Iterable = None, l_range: Iterable = None):
-        if m_range is None:
-            m_range = range(10)
+    def __init__(self, spine_mesh: Polyhedron_3 = None, l_range: Iterable = None):
         if l_range is None:
             l_range = range(10)
-        self._basis = [self._get_basis(l, m) for l in l_range for m in m_range]
+        self._basis = [self._get_basis(m, l) for l in l_range for m in range(-l, l+1)]
         super().__init__(spine_mesh)
 
     def surface_norm(self, point: List[float], dx: float = 0.01) -> np.ndarray:
@@ -71,8 +74,8 @@ class SphericalGarmonicsSpineMetric(ApproximationSpineMetric):
         norm = np.cross(ab, ac)
 
         norm = np.matmul(np.array([
-            [point_sin[0]*point_cos[1], np.dot(point_cos), -point_sin[1]],
-            [np.dot(point_sin), point_cos[0]*point_sin[1], point_cos[1]],
+            [point_sin[0]*point_cos[1], point_cos[0]*point_cos[1], -point_sin[1]],
+            [point_sin[0]*point_sin[1], point_cos[0]*point_sin[1], point_cos[1]],
             [point_cos[0], -point_sin[0], 0]
         ]), norm)
         return norm / np.linalg.norm(norm)
@@ -80,29 +83,34 @@ class SphericalGarmonicsSpineMetric(ApproximationSpineMetric):
     def _get_mesh(self, steps_num: List[int]) -> Polyhedron_3:
         theta = np.linspace(0, np.pi, steps_num[0])
         phi = np.linspace(0, 2 * np.pi, steps_num[1])
-        radiuses = np.array([self.surface_value([t, p]) for p in phi for t in theta])
-        xyz = polar2cart(theta, phi, radiuses)
-        normals = [self.surface_norm([t, p]) for p in phi for t in theta]
+        tp = np.concatenate(np.stack(np.meshgrid(theta, phi), axis=-1))
+        radiuses = np.array([real(self.surface_value([t, p])) for t, p in tp])
+        xyz = polar2cart(tp[...,0], tp[...,1], radiuses)
+        normals = [self.surface_norm([t, p]) for t, p in tp]
         out: Point_set_3 = Point_set_3()
         out.add_normal_map()
         for p, n in zip(xyz, normals):
-            out.insert(p, n)
+            out.insert(list_2_point(p), Vector_3(n[0], n[1], n[2]))
         surface_poly = Polyhedron_3()
         return poisson_surface_reconstruction(out, surface_poly)
+
+    def _get_mesh_2(self):
+        pass
 
     def _get_basis(self, order: int, degree: int) -> callable:
         return lambda theta, phi: sph_harm(order, degree, theta, phi)
 
     def _calculate(self, spine_mesh: Polyhedron_3) -> np.ndarray:
         v = spine_mesh.vertices()
-        radius_theta_psi = [cart2polar(*point_2_list(vertex.point())) for vertex in v]
+        points = [point_2_list(vertex.point()) for vertex in v]
+
+        center = (np.min(points, axis=0) + np.max(points, axis=0)) / 2
+        radius_theta_psi = [cart2polar(*(p - center)) for p in points]
+
         a_matrix = [[real(b(*point[1:])) for b in self._basis] for point in radius_theta_psi]
         b_matrix = [point[0] for point in radius_theta_psi]
-        #lr = LinearRegression()
-        #lr.fit(a_matrix, b_matrix)
         res = np.linalg.lstsq(a_matrix, b_matrix)[0]
-        #res = lr.coef_
-        self._coefficients = res.tolist()
+        self._value = res.tolist()
         return res
 
     @classmethod

spine_analysis/shape_metric/io_metric.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Union, Any
 import numpy as np
 
 from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
+from spine_analysis.shape_metric.approximation_metric import ApproximationSpineMetric
 from spine_analysis.shape_metric.float_metric import FloatSpineMetric
 from spine_analysis.shape_metric.metric_core import SpineMetric
 from spine_analysis.shape_metric.utils import calculate_metrics, create_metric_by_name
@@ -47,6 +48,9 @@ class SpineMetricDataset:
     def column(self, metric_name: str) -> List[SpineMetric]:
         return self._table[:, self._metric_2_column[metric_name]]
 
+    def element(self, spine_name: str, metric_name: str) -> SpineMetric:
+        return self._table[self._spine_2_row[spine_name], self._metric_2_column[metric_name]]
+
     @property
     def spine_names(self) -> List[str]:
         return list(self._spine_2_row.keys())
@@ -78,20 +82,22 @@ class SpineMetricDataset:
 
     def standardize(self) -> None:
         float_metric_indices = [i for i in range(self.num_of_metrics)
-                                if isinstance(self._table[0, i], FloatSpineMetric)]
+                                if isinstance(self._table[0, i], FloatSpineMetric)
+                                or isinstance(self._table[0,i], ApproximationSpineMetric)]
 
         # calculate mean and std by column
         mean = {}
         std = {}
         for i in float_metric_indices:
-            column_values = [metric.value for metric in self._table[:, i]]
-            mean[i] = np.mean(column_values)
-            std[i] = np.std(column_values)
+            column_values = np.array([metric.value for metric in self._table[:, i]])
+            mean[i] = np.mean(column_values, axis=0)
+            std[i] = np.std(column_values, axis=0)
 
         for i in range(self.num_of_spines):
             for j in float_metric_indices:
                 metric = self._table[i, j]
                 metric.value = (metric.value - mean[j]) / std[j]
+
 
     def standardized(self) -> "SpineMetricDataset":
         output = deepcopy(self)