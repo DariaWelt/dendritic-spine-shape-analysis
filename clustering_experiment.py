21a6d94f9cc8dafba353f0eba107baf2c90d2d02
daria <dsmirnova@itsociety.su>
2022-09-04 22:41:48 +0300

    - refactored clustering experiment script

--- clustering_experiment.py
+++ clustering_experiment.py
@@ -1,88 +1,115 @@
+from dataclasses import dataclass
+from typing import List, Any
+
 import matplotlib.pyplot as plt
 
+from spine_analysis.clusterization import SpineClusterizer
 from spine_analysis.clusterization.dbscan_clusterizer import DBSCANSpineClusterizer
 from spine_analysis.clusterization.kmeans_clusterizer import KMeansSpineClusterizer
 from spine_analysis.clusterization.loaded_clusterizer import ManualSpineClusterizer
 from spine_analysis.mesh.utils import load_spine_meshes
 from spine_analysis.shape_metric.io_metric import SpineMetricDataset
-from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
 import numpy as np
 from scipy.special import kl_div
 from notebook_widgets import create_dir
 
 
-metric_names = ["SphericalGarmonics", "OpenAngle", "CVD", "AverageDistance",
-                "Length", "Area", "Volume", "ConvexHullVolume", "ConvexHullRatio"]
+@dataclass()
+class ClusterizerInfo:
+    name: str
+    param_str: str
+    param_value: Any
+    clusterizer_instance: SpineClusterizer
 
-folder = '0.025-0.025-0.1-dataset'
-# load meshes
-full_mesh = Polyhedron_3(f"{folder}/3_full_res (1)/surface_mesh.off")
-spine_meshes = load_spine_meshes(folder_path=folder)
 
-every_spine_metrics = SpineMetricDataset()
-every_spine_metrics.calculate_metrics(spine_meshes, metric_names)
+@dataclass()
+class ClusteringExperiment:
+    metrics: List[str]
+    clusterizers: List[ClusterizerInfo]
+
+
+dataset_folder = '0.025-0.025-0.1-dataset'
 output_dir = "output/clustering_normalized"
+calculate_metrics = True
+save_metrics = True
+standardize_metrics = True
+
+param1 = lambda x: 0.1 * (x + 1)
+param2 = lambda x: 0.01 * (x + 1)
+
+experiments = [
+    ClusteringExperiment(
+        ["OldChordDistribution"],
+        [
+            ClusterizerInfo("DBSCAN_kldiv", f"e={(param1(i)):.2f})", param1(i),
+                            DBSCANSpineClusterizer(metric=lambda x, y: np.sum(kl_div(x, y)), eps=param1(i)))
+            for i in range(100)
+        ]),
+    ClusteringExperiment(
+        ["SphericalGarmonics"],
+        [
+            ClusterizerInfo("DBSCAN_l1", f"e={(param2(i)):.2f})", param2(i),
+                            DBSCANSpineClusterizer(metric="l1", eps=param2(i)))
+            for i in range(100)] +
+        [
+            ClusterizerInfo("KMeans_l1", f"n={i}", i,
+                            KMeansSpineClusterizer(num_of_clusters=i, metric="l1"))
+            for i in range(2, 30)
+        ]),
+    ClusteringExperiment(
+        ["OpenAngle", "CVD", "AverageDistance", "Length", "Area", "Volume", "ConvexHullVolume", "ConvexHullRatio"],
+        [
+            ClusterizerInfo("DBSCAN_l2", f"e={(param2(i)):.2f})", param2(i),
+                            DBSCANSpineClusterizer(metric="l2", eps=param2(i)))
+            for i in range(100)] +
+        [
+            ClusterizerInfo("KMeans_l2", f"n={i}", i,
+                            KMeansSpineClusterizer(num_of_clusters=i, metric="l2"))
+            for i in range(2, 30)
+        ]),
+]
+
 create_dir(output_dir)
-every_spine_metrics.export_to_csv(f"{output_dir}/metrics.csv")
 
-every_spine_metrics = SpineMetricDataset.load_metrics(f"{output_dir}/metrics.csv")
-every_spine_metrics.standardize()
+# load meshes
+spine_meshes = load_spine_meshes(folder_path=dataset_folder)
 
-every_spine_metrics.standardize()
+if calculate_metrics:
+    every_spine_metrics = SpineMetricDataset()
+    every_spine_metrics.calculate_metrics(spine_meshes, list({m for e in experiments for m in e.metrics}))
 
-#index_subsets = [list(range(8))]
-#index_subsets = [[0]]
-index_subsets = [[1, 2, 3, 4, 5, 6, 8]]
+    if save_metrics:
+        every_spine_metrics.save(f"{output_dir}/metrics.csv")
 
-# clusterizers
-all_clusterizers = []
+every_spine_metrics = SpineMetricDataset.load(f"{output_dir}/metrics.csv")
 
-# dbscan
-# for (name, metric) in zip(["euclidean", "kl_div"],
-#                           ["euclidean", lambda x, y: np.sum(kl_div(x, y))]):
-for i in range(100):
-    eps = 0.01 * (i + 1) #pow(10,(i+1))
-    all_clusterizers.append(("DBSCAN_l1", f"e={0.01*(i+1)})", eps,
-                             DBSCANSpineClusterizer(metric="l1", eps=eps)))
-for i in range(100):
-    eps = 0.1 * (i + 1)
-    all_clusterizers.append(("DBSCAN_kldiv", f"e={eps:.2f}", eps,
-                             DBSCANSpineClusterizer(metric=lambda x, y: np.sum(kl_div(x, y)), eps=eps)))
+if standardize_metrics:
+    every_spine_metrics.standardize()
 
-#kmeans
-for num_of_clusters in range(2, 30):
-    all_clusterizers.append(("KMeans_l1", f"n={num_of_clusters}", num_of_clusters,
-                             KMeansSpineClusterizer(num_of_clusters=num_of_clusters, metric="l1")))
+for experiment in experiments:
+    reduced_metrics = every_spine_metrics.get_metrics_subset(experiment.metrics)
 
-# all possible metric combinations
-# for L in range(1, len(metric_names) + 1):
-#     for index_subset in itertools.combinations(list(range(len(metric_names))), L):
-for index_subset in index_subsets:
-    reduced_metric_names = [metric_names[i] for i in index_subset]
-    reduced_metrics = every_spine_metrics.get_metrics_subset(reduced_metric_names)
-
-    base_save_dir = f"{output_dir}/{str(reduced_metric_names)}"
+    base_save_dir = f"{output_dir}/{str(experiment.metrics)}"
     create_dir(base_save_dir)
 
-    scores = {"KMeans": ([], []), "KMeans_l1": ([], []),
-              "DBSCAN_eucl": ([], []), "DBSCAN_l1": ([], [])}
+    scores = {c.name: ([], []) for c in experiment.clusterizers}
 
-    for (clusterizer_name, param_string, param_value, clusterizer) in all_clusterizers:
-        save_dir = f"{base_save_dir}/{clusterizer_name}"
+    for c in experiment.clusterizers:
+        save_dir = f"{base_save_dir}/{c.name}"
         create_dir(save_dir)
 
-        clusterizer.fit(reduced_metrics)
-        score = clusterizer.score()
-        if np.isnan(score):  # or score <= 0:
-            continue
+        c.clusterizer_instance.fit(reduced_metrics)
+        #score = c.clusterizer_instance.score()
+        #if np.isnan(score):  # or score <= 0:
+        #    continue
 
         # save plot result
-        filename = f"{save_dir}/{clusterizer_name}_{param_string}_score={score:.2f}"
-        clusterizer.save_plot(filename + ".png")
-        clusterizer.save(filename + ".json")
+        filename = f"{save_dir}/{c.name}_{c.param_str}"
+        c.clusterizer_instance.save_plot(filename + ".png")
+        c.clusterizer_instance.save(filename + ".json")
 
-        scores[clusterizer_name][0].append(param_value)
-        scores[clusterizer_name][1].append(score)
+        scores[c.name][0].append(c.param_value)
+        #scores[c.name][1].append(score)
 
     for clusterizer_name in scores:
         plt.axhline(y=0, color='r', linestyle='-')
