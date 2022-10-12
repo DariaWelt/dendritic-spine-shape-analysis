Light Field: - fixed moments visualization - fixed moments calculation - added reconstruction tests

spine_analysis/shape_metric/approximation_metric.py
@@ -6,7 +6,6 @@ from typing import List, Iterable, Any, Tuple
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import mahotas
 import meshplot as mp
 from ipywidgets import widgets
 from lfd import LightFieldDistance
@@ -150,8 +149,7 @@ class LightFieldZernikeMomentsSpineMetric(SpineMetric):
     def _calculate(self, spine_mesh: Polyhedron_3) -> Any:
         self._value = []
         for projection in self.get_projections(spine_mesh):
-            self._value.append(mahotas.features.zernike_moments(projection, self._zernike_radius,
-                                                                degree=self._zernike_order).tolist())
+            self._value.append(self._calculate_moment(projection, degree=self._zernike_order).real.tolist())
         return self._value
 
     def _get_image(self, normal: list, polyline: Tuple[Point_3]):
@@ -254,26 +252,25 @@ class LightFieldZernikeMomentsSpineMetric(SpineMetric):
         result = np.zeros(len(circle_mask), dtype=complex)
         Y, X = Y[circle_mask], X[circle_mask]
         computed = np.zeros(len(Y), dtype=complex)
-        n, l = 0, 0
-        for i, a in enumerate(zernike_moments):
-            vxy = self.zernike_poly(Y, X, n, l)
-            computed += a * vxy
-            if n > l:
-                l += 1
-            else:
-                n += 1
-                l = 0
-        computed = computed - min(computed) #/ (max(computed) - min(computed))
+        i = 0
+        n = 0
+        while i < len(zernike_moments):
+            for l in range(n + 1):
+                if (n - l) % 2 == 0:
+                    vxy = self.zernike_poly(Y, X, n, l)
+                    computed += zernike_moments[i] * vxy
+                    i += 1
+            n += 1
+        #computed = computed - min(computed) #/ (max(computed) - min(computed))
         result[circle_mask] = computed
         return result.reshape((image_size,)*2).real
 
-    def zernike_poly(Y, X, n, l):
-        l_negative = l < 0
+    def zernike_poly(self, Y, X, n, l):
         l = abs(l)
         if (n - l) % 2 == 1:
             return np.zeros(len(Y), dtype=complex)
         Rho, _, Phi = cart2polar(X, Y, np.zeros(len(X)))
-        multiplier = np.sin(l * Phi) if l_negative else np.cos(l * Phi)
+        multiplier = (1.*np.cos(Phi) + 1.j*np.sin(Phi)) ** l
         radial = np.sum([(-1.) ** m * factorial(n - m) /
                          (factorial(m) * factorial((n + l - 2 * m) // 2) * factorial((n - l - 2 * m) // 2)) *
                          np.power(Rho, n - 2 * m)
@@ -281,4 +278,23 @@ class LightFieldZernikeMomentsSpineMetric(SpineMetric):
                         axis=0)
         return radial * multiplier
 
+    def _calculate_moment(self, image: np.ndarray, degree: int = 8):
+        radius = image.shape[0] // 2
+        moments = []
+        Y, X = np.meshgrid(range(image.shape[0]), range(image.shape[1]))
+        Y, X = ((Y - radius) / radius).ravel(), ((X - radius) / radius).ravel()
+
+        circle_mask = (np.sqrt(X ** 2 + Y ** 2) <= 1)
+        Y, X = Y[circle_mask], X[circle_mask]
+
+        frac_center = np.array(image.ravel()[circle_mask], np.double)
+        frac_center /= frac_center.sum()
+
+        for n in range(degree + 1):
+            for l in range(n + 1):
+                if (n - l) % 2 == 0:
+                    vxy = self.zernike_poly(Y, X, n, l)
+                    moments.append(sum(frac_center * np.conjugate(vxy)) * (n + 1)/np.pi)
+
+        return np.array(moments)
 

test/test_light_field_visualization.py
@@ -1,6 +1,6 @@
 from typing import List
 
-import mahotas
+import cv2
 import numpy as np
 import pytest
 
@@ -16,12 +16,60 @@ class ZernikeVisualizationCase(TestCaseBase):
         self.zernike_moments = zernike_moments
 
 
+class ZernikeReconstructionCase(TestCaseBase):
+    threshold_value: float
+    fp_scale: float
+
+    def __init__(self, name: str, image: np.ndarray, mask_threshold: float = 0.35, fp_scale: float = 0.05):
+        super(ZernikeReconstructionCase, self).__init__(name, image)
+        self.threshold_value = mask_threshold
+        self.fp_scale = fp_scale
+
+    @staticmethod
+    def _draw_polygon(contour) -> np.ndarray:
+        output = np.zeros((200, 200))
+        cv2.fillPoly(output, pts=[contour], color=(1, 1, 1))
+        return output
+
+    @staticmethod
+    def get_stick_image() -> np.ndarray:
+        contour = np.array([[50, 50], [150, 150], [145, 140], [50, 40]])
+        return ZernikeReconstructionCase._draw_polygon(contour)
+
+    @staticmethod
+    def get_cell_polygon() -> np.ndarray:
+        contour = np.array([[40, 40], [35, 57], [30, 60], [40, 100], [140, 160], [146, 147], [160, 50], [170, 40],
+                            [50, 30]])
+        return ZernikeReconstructionCase._draw_polygon(contour)
+
 @pytest.mark.parametrize('case', [
                              ZernikeVisualizationCase(name='circle', zernike_moments=[1] + [0] * 24),
                              ZernikeVisualizationCase(name='Z_1^1', zernike_moments=[0] + [1] + [0] * 23)
                          ], ids=str)
-def test_reconstruction(case: ZernikeVisualizationCase):
+def test_exact_reconstruction(case: ZernikeVisualizationCase):
     light_field_metric = LightFieldZernikeMomentsSpineMetric()
     res = light_field_metric._recover_projection(case.zernike_moments, 200)
-    res_moments = mahotas.features.zernike_moments(res, 2, degree=8).tolist()
-    assert res_moments == case.zernike_moments
+    res_moments = light_field_metric._calculate_moment(res, degree=8).real
+    assert res_moments.tolist() == case.zernike_moments
+
+
+@pytest.mark.parametrize('case', [
+                             ZernikeReconstructionCase(name='stick', image=ZernikeReconstructionCase.get_stick_image()),
+                             ZernikeReconstructionCase(name='polygon', image=ZernikeReconstructionCase.get_cell_polygon())
+                         ], ids=str)
+def test_approx_reconstruction(case: ZernikeReconstructionCase):
+    light_field_metric = LightFieldZernikeMomentsSpineMetric()
+    zernike_moments = light_field_metric._calculate_moment(case._gt, degree=20).tolist()
+    res = light_field_metric._recover_projection(zernike_moments, 200)
+
+    res = res / abs(res).max()
+    res[res < case.threshold_value] = 0
+    res[res > 0] = 1
+
+    overlap = res * case._gt  # Logical AND
+    union = res + case._gt  # Logical OR
+    union[union > 0] = 1
+    sym_dif = union - overlap
+
+    fp = sym_dif.sum() / (res.shape[0] * res.shape[1])
+    assert fp <= case.fp_scale